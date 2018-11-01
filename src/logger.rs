8a822526c72dc561ddc4177371a8079218a8fd21
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-01 15:46:05 -0700

    Extract raw_logger and json_logger into separate modules

--- src/logger.rs
+++ src/logger.rs
@@ -1,84 +1,96 @@
-extern crate log;
+use log::Level;
+use settings::Logging;
 
-use std::io::prelude::*;
-use std::io::{self, Stdout};
-use std::borrow::ToOwned;
-use std::str;
-use log::{Log,Level,Metadata,Record,SetLoggerError};
+mod raw_logger {
+    use log::{set_boxed_logger, set_max_level, Level, Log, Metadata, Record, SetLoggerError};
 
-struct SimpleLogger {
-    name: String,
-    level: Level,
-}
-
-impl Log for SimpleLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= self.level
+    struct SimpleLogger {
+        name: String,
+        level: Level,
     }
 
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!(
-                "{} {:<5} [{}] {}",
-                self.name,
-                record.level().to_string(),
-                record.module_path().unwrap_or_default(),
-                record.args());
+    impl Log for SimpleLogger {
+        fn enabled(&self, metadata: &Metadata) -> bool {
+            metadata.level() <= self.level
         }
+
+        fn log(&self, record: &Record) {
+            if self.enabled(record.metadata()) {
+                println!(
+                    "{} {:<5} [{}] {}",
+                    self.name,
+                    record.level().to_string(),
+                    record.module_path().unwrap_or_default(),
+                    record.args()
+                );
+            }
+        }
+
+        fn flush(&self) {}
     }
 
-    fn flush(&self) {
+    pub fn init(name: &str, level: Level) -> Result<(), SetLoggerError> {
+        let logger = SimpleLogger {
+            name: name.to_owned(),
+            level: level,
+        };
+
+        set_boxed_logger(Box::new(logger))?;
+        set_max_level(level.to_level_filter());
+
+        Ok(())
     }
 }
 
-pub fn init_raw(name: &str, level: Level) -> Result<(), SetLoggerError> {
-    let logger = SimpleLogger {
-        name: name.to_owned(),
-        level: level
-    };
-    log::set_boxed_logger(Box::new(logger))?;
-    log::set_max_level(level.to_level_filter());
-    Ok(())
-}
+mod json_logger {
+    use log::{set_boxed_logger, set_max_level, Level, Log, Metadata, Record, SetLoggerError};
 
-pub struct JsonLogger {
-    out: Stdout,
-    level: Level,
-    name: String,
-}
-
-impl Log for JsonLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= self.level
+    pub struct JsonLogger {
+        level: Level,
+        name: String,
     }
 
-    fn log(&self, record: &Record) {
-        let output = json!({
-            "level": record.level().to_string(),
-            "name": self.name,
-            "src": {
-                "module_path": record.module_path().unwrap_or_default(),
-                "file": record.file(),
-                "line": record.line()
-            },
-            "msg": record.args().to_string()
-        });
+    impl Log for JsonLogger {
+        fn enabled(&self, metadata: &Metadata) -> bool {
+            metadata.level() <= self.level
+        }
 
-        writeln!(&mut self.out.lock(), "{}", output.to_string());
+        fn log(&self, record: &Record) {
+            let json_record = json!({
+                "level": record.level().to_string(),
+                "name": self.name,
+                "src": {
+                    "module_path": record.module_path().unwrap_or_default(),
+                    "file": record.file(),
+                    "line": record.line()
+                },
+                "msg": record.args().to_string()
+            });
+
+            if self.enabled(record.metadata()) {
+                println!("{}", json_record.to_string());
+            }
+        }
+
+        fn flush(&self) {}
     }
 
-    fn flush(&self) {}
+    pub fn init(name: &str, level: Level) -> Result<(), SetLoggerError> {
+        let logger = JsonLogger {
+            level: level,
+            name: name.to_owned(),
+        };
+
+        set_boxed_logger(Box::new(logger))?;
+        set_max_level(level.to_level_filter());
+
+        Ok(())
+    }
 }
 
-pub fn init_json(name: &str, level: Level) -> Result<(), SetLoggerError> {
-    let logger = JsonLogger {
-        out: io::stdout(),
-        level: level,
-        name: name.to_owned(),
-    };
-
-    log::set_boxed_logger(Box::new(logger))?;
-    log::set_max_level(level.to_level_filter());
-
-    Ok(())
+pub fn init_logger(log_type: Logging, name: &str, level: Level) {
+    match log_type {
+        Logging::Raw => raw_logger::init(name, level),
+        Logging::Json => json_logger::init(name, level),
+    }.unwrap()
 }

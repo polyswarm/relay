5b6b0fd0e3aaebdd93f5dfa6599521a653689ccf
sam <samuel.neto@polyswarm.io>
2019-06-13 18:49:11 -0300

    adding CI stages, running lint, test and coverage inside image

---
+++ coverage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt-get update
+apt-get install -y jq zsh binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev unzip cmake build-essential
+rustup update nightly # required for kcov instrumentation
+rustup default nightly
+cargo install cargo-cov
+./testutils/collect_coverage.sh --auto
---
+++ lint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+rustup component add rustfmt
+cargo fmt --all -- --check
+rustup component add clippy
+cargo clippy

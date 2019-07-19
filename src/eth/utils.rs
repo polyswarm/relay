2edb0b5fdbdbb5eeb7da37dde90fdfd1400752e0
Rob Lathrop <rl@polyswarm.io>
2019-07-19 09:02:47 -0400

    Add tests to clean_0x

--- src/eth/utils.rs
+++ src/eth/utils.rs
@@ -6,3 +6,26 @@ pub fn clean_0x(s: &str) -> &str {
         s
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn removes_0x_from_addr() {
+        // arrange
+        // act
+        let result = clean_0x("0xstring");
+        // assert
+        assert_eq!(result, "string");
+    }
+
+    #[test]
+    fn no_mods_if_no_0x() {
+        // arrange
+        // act
+        let result = clean_0x("string");
+        // assert
+        assert_eq!(result, "string");
+    }
+}
